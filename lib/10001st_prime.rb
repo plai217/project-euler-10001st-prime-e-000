140ca55f5619eb0f36d8e7d973f74408dbe96b32
plai217 <philiplai217@gmail.com>
2015-10-27 13:02:15 -0400

    Done.

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,23 @@
 # Implement your procedural solution here!
+def prime_number_for(num)
+
+  nthprime = 0
+  counter = 2
+
+  until nthprime == num
+    if prime(counter)
+      nthprime += 1
+    end
+    counter +=1
+  end
+  counter - 1
+end
+
+def prime(num)
+  Math.sqrt(num).to_i.downto(2) do |x|
+    if num % (x) == 0 
+      return false
+    end
+  end
+  return true
+end
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,30 @@
 # Implement your object-oriented solution here!
+class Prime
+
+  def initialize(nthprime)
+    @nthprime = nthprime
+  end
+
+  def number
+    nthprime = 0
+    counter = 2
+
+    until nthprime == @nthprime
+      if prime(counter)
+        nthprime += 1
+      end
+      counter +=1
+    end
+    counter - 1
+  end
+
+  def prime(num)
+    Math.sqrt(num).to_i.downto(2) do |x|
+      if num % (x) == 0 
+        return false
+      end
+    end
+    return true
+  end
+
+end
